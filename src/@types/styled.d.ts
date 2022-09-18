15f50f8dc7a4ea937e6567473ac36d5782349ccb
PatiSacramento <patricia.sacramento@labenu.com.br>
2022-09-17 21:53:22 -0300

    first commit

---
+++ src/@types/styled.d.ts
@@ -0,0 +1,10 @@
+// arquivo de definição de tipos
+
+// import styled from 'styled-components'
+import { defaultTheme } from '../styles/themes/default'
+
+type ThemeType = typeof defaultTheme
+
+declare module 'styled-components' {
+  export interface DefaultTheme extends ThemeType {}
+}
